1841aece0592b5cd22538d19339a87934f089de2
violeta atkinson <violeta.atkinson@gmail.com>
2022-10-17 11:46:22 +0200

    home done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
-import Login from './screens/Login/Login'
+import Home from './screens/Home/Home';
+
+
+<link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Telugu:wght@100&display=swap" rel="stylesheet"></link>
 
 function App() {
   return (
     <div className="App">
       
       <Routes>
-          <Route path='/' element={<Login />}/>
+          <Route path='/' element={<Home />}/>
         </Routes>
     </div>
   );
---
+++ src/screens/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import home from "./Home.css";
+import Cat from "../../assets/cat.png";
+import { Link } from "react-router-dom";
+
+export default function Home() {
+	return (
+		<div className="home">
+			<img src={Cat} alt="logo" height={190} />
+			<h1>Beautiful Souls</h1>
+			<p className="home-p">Don't have an account yet ?</p>
+			<Link className="link" to="/signup">Register</Link>
+			<p>Do you have an account ?</p>
+			<Link className="link-1" to="/login">Login</Link>
+		</div>
+	);
+}
